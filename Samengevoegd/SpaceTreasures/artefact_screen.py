841a23bb6fdb3a9e50e206a4ef5225680c3e5884
VACEfron <46462862+VACEfron@users.noreply.github.com>
2021-01-12 14:19:31 +0100

    Tekst veranderd

--- Samengevoegd/SpaceTreasures/artefact_screen.py
+++ Samengevoegd/SpaceTreasures/artefact_screen.py
@@ -35,9 +35,9 @@ def draw():
     if rollFirstTime == True:
         fill(255)        
         textSize(18)        
-        text("Dobbel in het echt of\nklik op de dobbelsteen\nom te rollen en kijk\nof je de artefact\nmag toevoegen!", 20, 115)
+        text("Klik op de dobbelsteen\nom te rollen en kijk\nof je het artefact\nmag toevoegen", 22, 135)
     
-    if roll_dice and dice_roll_time > 0 and frame % 2 == 0: 
+    if roll_dice and dice_roll_time > 0 and frame % 5 == 0: 
         display_dice = int(random(1, 7)) - 1  
         dice_roll_time -= 5
 
@@ -131,7 +131,7 @@ def draw():
     elif isMouseOnButton(815, 350, 55, 55) and elementIndex < 2:
         image(PijlVerder2, 815, 350, 55, 55)
         cursor(HAND)
-    elif isMouseOnButton(90, 200, 200, 200):
+    elif isMouseOnButton(90, 220, 200, 200):
         cursor(HAND)
     else: cursor(ARROW)
 
